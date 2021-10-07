74cef8a870a5f9ddd2960ae8b99615de4935eaaf
kickscar <kickscar@gmail.com>
2021-10-07 20:04:55 +0900

    @Order 적용

--- ch02/src/test/java/_02/Ex06.java
+++ ch02/src/test/java/_02/Ex06.java
@@ -6,13 +6,13 @@ import org.apache.ibatis.jdbc.ScriptRunner;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import javax.sql.DataSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class Ex06 {
     private static SqlSessionFactory sqlSessionFactory;
 
@@ -26,14 +26,17 @@ public class Ex06 {
     }
 
     @Test
+    @Order(1)
     public void testInsertSQL()  {
         SqlSession session = sqlSessionFactory.openSession();
         int count = session.insert("book.insert", "마이바티스 연습");
+        session.commit();
 
         assertEquals(1, count);
     }
 
     @Test
+    @Order(2)
     public void testSelectSQL()  {
         SqlSession session = sqlSessionFactory.openSession();
         Book book = session.selectOne("book.findByNo", 1L);
